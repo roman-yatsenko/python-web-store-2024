13f615eff118eac583285f265185eab5538c42c2
YaRo <roman.yatsenko@hneu.net>
2024-12-04 19:16:15 +0200

    Add cart styles

--- cart/forms.py
+++ cart/forms.py
@@ -3,6 +3,6 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='Кількість:')
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
     
