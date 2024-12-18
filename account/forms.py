dd30f155568dc4e3bc329a7e8a5f273727077bab
YaRo <roman.yatsenko@hneu.net>
2024-12-18 19:06:51 +0200

    Add registration

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Логін')
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторити пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
     
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Паролі не співпадають.')
+        return cd['password2']
--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('register/', views.register, name='register'),
 ]
