c79a0ad036c870bb7cc011d807e6cdc0bc8ffb81
YaRo <roman.yatsenko@hneu.net>
2025-01-29 18:45:17 +0200

    Add orders export in csv

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,37 @@
+import csv
+from datetime import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
 
 # Register your models here.
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # запис першого рядку з інформацією заголовку
+    writer.writerow([field.verbose_name for field in fields])
+    # запис рядків даних
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime):
+                value = value.strftime("%d.%m.%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = "Експорт в CSV"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -18,3 +47,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
