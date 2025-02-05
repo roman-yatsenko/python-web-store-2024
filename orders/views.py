e4901bc8c95e0b883106245b6d4bc190aabe2112
YaRo <roman.yatsenko@hneu.net>
2025-02-05 18:22:53 +0200

    Add admin order view

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, render
 
 from cart.cart import Cart
 
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import Order, OrderItem
 
 # Create your views here.
 
@@ -32,3 +33,9 @@ def order_create(request):
             initial['email'] = request.user.email
         form = OrderCreateForm(initial=initial)
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
