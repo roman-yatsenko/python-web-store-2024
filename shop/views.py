432a70f636d432afc6221107263b89aba4f8a929
YaRo <roman.yatsenko@hneu.net>
2025-03-05 19:51:54 +0200

    Update views for parler models

--- shop/views.py
+++ shop/views.py
@@ -13,10 +13,15 @@ def product_list(request: HttpRequest, category_slug: str=None) -> HttpResponse:
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
+        language = request.LANGUAGE_CODE
+        category = get_object_or_404(
+            Category,
+            translations__language_code=language, 
+            translations__slug=category_slug
+        )
         products = products.filter(category=category)
     if query := request.GET.get('search'):
-        products = products.filter(name__contains=query) | products.filter(description__contains=query)
+        products = products.filter(translations__name__contains=query) | products.filter(translations__description__contains=query)
     return render(
         request, 
         'shop/product/list.html',
@@ -28,6 +33,8 @@ def product_list(request: HttpRequest, category_slug: str=None) -> HttpResponse:
 
 def product_detail(request: HttpRequest, id: int, slug: str) -> HttpResponse:
     """View для одного товару"""
-    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    language = request.LANGUAGE_CODE
+    product = Product.objects.filter(id=id, translations__slug=slug, available=True).first()
+    product.set_current_language(language)
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
