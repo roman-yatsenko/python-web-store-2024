5468263595759174d83959377691cb4f99742f46
YaRo <roman.yatsenko@hneu.net>
2025-03-05 18:20:06 +0200

    Add lang prefixes to urls

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
@@ -28,7 +29,7 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', include('shop.urls', namespace='shop')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
