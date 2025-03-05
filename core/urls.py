171296be622f1b4a441991452b0d023463971b89
YaRo <roman.yatsenko@hneu.net>
2025-03-05 18:37:00 +0200

    Add url translations

--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,13 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
-    path('account/', include('account.urls')),
+    path(_('account/'), include('account.urls')),
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
     path('coupons/', include('coupons.urls', namespace='coupons')),
     path('rosetta/', include('rosetta.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
